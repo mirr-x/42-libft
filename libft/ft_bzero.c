bbbfb065d4e24538425cb54920570e66d79fb941
mirr-x <amir.exe@icloud.com>
2025-10-21 12:57:54 +0100

    Update ft_bzero and ft_isalnum with additional comments and modify timestamps

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,12 +6,13 @@
 /*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 21:41:16 by molahrac          #+#    #+#             */
-/*   Updated: 2025/10/19 21:54:51 by molahrac         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:59:26 by molahrac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* Sets the first 'n' bytes of memory at 's' to zero */
 void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,12 +6,13 @@
 /*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 12:43:49 by molahrac          #+#    #+#             */
-/*   Updated: 2025/10/18 13:32:30 by molahrac         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:59:52 by molahrac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* Checks if 'c' is an alphanumeric character (a letter or a digit) */
 int	ft_isalnum(int c)
 {
 	if (!(ft_isalpha(c) || ft_isdigit(c)))
