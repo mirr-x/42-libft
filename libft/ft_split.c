3e342e4498f4e25999cd37b186295ee8ab3b40ec
mirr-x <amir.exe@icloud.com>
2025-10-26 10:27:13 +0100

    this is my new slpit v.2 version see prev versio to see diff

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,18 +6,16 @@
 /*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:14:18 by molahrac          #+#    #+#             */
-/*   Updated: 2025/10/26 01:29:24 by molahrac         ###   ########.fr       */
+/*   Updated: 2025/10/26 10:26:14 by molahrac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-//+-------------count words
 int	count_words(char const *s, char c)
 {
-	int	i;
-	int	counter;
+	int		i;
+	int		counter;
 
 	i = 0;
 	counter = 0;
@@ -25,7 +23,7 @@ int	count_words(char const *s, char c)
 	{
 		if (s[i] != c)
 		{
-			counter++; //@we found a word
+			counter++;
 			while (s[i] != c && s[i] != '\0')
 				i++;
 		}
@@ -35,22 +33,36 @@ int	count_words(char const *s, char c)
 	return (counter);
 }
 
-//+------free the remian case of failing
+char	*ft_substrr(char const *str, int start, int end)
+{
+	char	*ptr;
+	int		s_len;
+	int		i;
+
+	s_len = end - start + 1;
+	ptr = (char *)malloc(s_len + 1);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i != s_len)
+	{
+		ptr[i++] = str[start++];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
 void	free_the_remain(char **arr2d, int num)
 {
 	int		i;
 
 	i = 0;
 	while (i != num)
-	{
-		free(arr2d[i]);
-		i++;
-	}
+		free(arr2d[i++]);
 	free(arr2d);
 }
 
-//+--------------extact words and copy them to the 2d arr
-char **extact_words_and_copy_them_to_the_2d_arr(char **arr2d, char const *s, char c, int num_of_words)
+char	**extact_wrds_to_2d_arr(char **arr2d, char const *s, char c, int n_wrds)
 {
 	int		i;
 	int		j;
@@ -59,65 +71,36 @@ char **extact_words_and_copy_them_to_the_2d_arr(char **arr2d, char const *s, cha
 
 	i = 0;
 	j = 0;
-	while (num_of_words != i && s[j] != '\0')
+	while (n_wrds != i)
 	{
-		
 		while (s[j] == c)
 			j++;
 		start = j;
 		while (s[j] != c && s[j] != '\0')
 			j++;
-		end = j;
-		// printf("s = %d and e = %d\n",start, end);
-		arr2d[i] = ft_substr(s, start, end - start);
-		if (arr2d[i] == NULL) // !fail
+		end = j - 1;
+		arr2d[i] = ft_substrr(s, start, end);
+		if (arr2d[i] == NULL)
 		{
 			free_the_remain(arr2d, i);
 			return (NULL);
 		}
-		// printf("____%s___\n",arr2d[i]);
 		i++;
 	}
 	return (arr2d);
 }
 
-	//+-------------split
 char	**ft_split(char const *s, char c)
 {
 	int		num_of_words;
 	char	**arr_of_words;
 
 	num_of_words = count_words(s, c);
-	// if (num_of_words == 0) // !fails
-	// 	return (malloc(1));
 	arr_of_words = (char **)malloc(((num_of_words + 1) * sizeof(char *)));
-	if(!arr_of_words) // !fails
+	if (!arr_of_words)
 		return (NULL);
 	arr_of_words[num_of_words] = NULL;
-	if (!extact_words_and_copy_them_to_the_2d_arr(arr_of_words, s, c, num_of_words))
+	if (!extact_wrds_to_2d_arr(arr_of_words, s, c, num_of_words))
 		return (NULL);
 	return (arr_of_words);
 }
-// !!hello!!!!!!!!!!"  s = 2  e = 6   len = 9 
-// //+-------------main
-
-// int main()
-// {
-// 	char *s = "          ";
-// 	char c = ' ';
-// 	char **arr_of_words = ft_split(s, c);
-// 	if (!arr_of_words)
-// 		return (0);
-// 	int i = 0;
-// 	if (arr_of_words != NULL)
-// 	{
-// 		while (arr_of_words[i] != NULL)
-// 		{
-// 			printf("_%s_\n",arr_of_words[i]);
-// 			free(arr_of_words[i]);
-// 			i++;
-// 		}
-// 		free(arr_of_words);
-// 	}
-// 	return(0);
-// }
