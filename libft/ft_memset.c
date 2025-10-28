2e3fa8fd78b4181234682eea26a42860967ae565
mirr-x <amir.exe@icloud.com>
2025-10-28 07:28:50 +0100

    Refactor ft_memset implementation and update Makefile and header for new functions

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,27 +6,24 @@
 /*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 15:41:12 by molahrac          #+#    #+#             */
-/*   Updated: 2025/10/26 13:32:02 by molahrac         ###   ########.fr       */
+/*   Updated: 2025/10/26 14:27:36 by molahrac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	rec(unsigned char *memory, unsigned char c, size_t len)
-{
-	if (len == 0)
-		return ;
-	rec(memory, c, len - 1);
-	memory[len - 1] = c;
-	return ;
-}
-
 /* Fills the first 'len' bytes of memory at 'b' with the value 'c' */
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
+	unsigned char	*memory;
+	size_t			i;
 
+	memory = (unsigned char *)b;
 	i = 0;
-	rec((unsigned char *)b, (unsigned char)c, (size_t)len);
+	while (i < len)
+	{
+		memory[i] = (unsigned char)c;
+		i++;
+	}
 	return (b);
 }
